75624613a274eda7a8f7c0609810e35209358f16
“Sushrit <sushrit.k@gmail.com>
2024-03-03 09:28:00 -0600

    changes app name

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ text-align: center;
 """
 
 # Display the centered header
-st.markdown("<h1 style='text-align: center;'>WATCH THIS!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>🍿 RANDOMFLIX 🎬</h1>", unsafe_allow_html=True)
 
 # def fetch_poster(movie_id):
 #     url = "https://api.themoviedb.org/3/movie/{}?api_key=1c827bec84375d8eab8f7bd655d17749".format(movie_id)
@@ -85,26 +85,70 @@ if st.button("Pick a Movie for Me!"):
         random_index = st.session_state.random_index = random.randint(0, len(movies_list) - 1)
     selected_movie_name.text("Selected Movie: " + movies_list[random_index])
     selectvalue = movies_list[random_index]
-    st.image(fetch_poster(selectvalue), width=250)
+    selectedimage = fetch_poster(selectvalue)
+    if selectedimage is not None:
+        st.image(selectedimage, width=250)
+    else:
+        st.write("No image available")
+    # st.image(fetch_poster(selectvalue), width=250)
+
+    # st.header("Similar Movies You Could Watch: ")
+    # movie_name, movie_poster = recommend(selectvalue)
+    # col1, col2, col3, col4, col5=st.columns(5) 
+    # with col1:
+    #     st.text(movie_name[0])
+    #     st.image(movie_poster[0])
+    # with col2:
+    #     st.image(movie_poster[1])
+    #     st.text(movie_name [1])
+    # with col3:
+    #     st.image(movie_poster[2])
+    #     st.text(movie_name [2])
+    # with col4:
+    #     st.image(movie_poster[3])
+    #     st.text(movie_name [3])
+    # with col5:
+    #     st.image(movie_poster[4])
+    #     st.text(movie_name[4])
 
     st.header("Similar Movies You Could Watch: ")
     movie_name, movie_poster = recommend(selectvalue)
-    col1, col2, col3, col4, col5=st.columns(5) 
+    col1, col2, col3, col4, col5 = st.columns(5) 
     with col1:
-        st.image(movie_poster[0])
+        if movie_poster[0] is not None:
+            st.image(movie_poster[0])
+        else:
+            st.write("No image available")
         st.text(movie_name[0])
+
     with col2:
-        st.image(movie_poster[1])
-        st.text(movie_name [1])
+        if movie_poster[1] is not None:
+            st.image(movie_poster[1])
+        else:
+            st.write("No image available")
+        st.text(movie_name[1])
+
     with col3:
-        st.image(movie_poster[2])
-        st.text(movie_name [2])
+        if movie_poster[2] is not None:
+            st.image(movie_poster[2])
+        else:
+            st.write("No image available")
+        st.text(movie_name[2])
+
     with col4:
-        st.image(movie_poster[3])
-        st.text(movie_name [3])
+        if movie_poster[3] is not None:
+            st.image(movie_poster[3])
+        else:
+            st.write("No image available")
+        st.text(movie_name[3])
+
     with col5:
-        st.image(movie_poster[4])
+        if movie_poster[4] is not None:
+            st.image(movie_poster[4])
+        else:
+            st.write("No image available")
         st.text(movie_name[4])
+
 
     # Call the recommendation model to get similar movies
     # index = movies[movies['title'] == movies_list[random_index]].index[0]
